66afcd0c36f90389d8dc316bfeaf1358cce7b8ae
zacki744 <78082188+zacki744@users.noreply.github.com>
2023-12-12 13:43:00 +0100

    .

--- components/search/search.js
+++ components/search/search.js
@@ -4,12 +4,8 @@ import { styles } from "./../../style/style.js";
 import { Feather, Entypo } from "@expo/vector-icons";
 import { CustomButton } from "./../obj/Button.js";
 import * as c from "./../../style/const.js";
-import { app, db, storage } from "./../../firebaseConfig.js";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-import { getStorage, ref, downloadBytes } from "firebase/storage";
-import { WebView } from "react-native-webview";
-import { Asset } from "expo-asset";
-import { Audio } from "expo-av";
+import { collection, getDocs } from "firebase/firestore/lite";
+
 
 async function getManuals(db) {
   const querySnapshot = await getDocs(collection(db, "Manuals"));
@@ -22,14 +18,7 @@ export default function Search() {
     clicked: false,
   });
   const { search, clicked } = state;
-  const [manuals, setmanuals] = useState({});
-  const pathReference = ref(
-    storage,
-    "manuals/vittsjoe-hylla-svartbrun-glas__AA-1388480-10-2.pdf"
-  );
-  const [pdfVisible, setPdfVisible] = useState(false);
-  const [pdfUri, setPdfUri] = useState(null);
-
+/*
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -40,7 +29,7 @@ export default function Search() {
     };
 
     fetchData();
-  }, []);
+  }, []);*/
 
   const setClicked = (ifClicked) => {
     setState({ ...state, clicked: ifClicked });
@@ -50,65 +39,39 @@ export default function Search() {
     setState({ ...state, search });
   };
 
-  const loadPdf = async () => {
-    try {
-      // Download the PDF from Firebase Storage
-      const pdfUrl = await getDownloadURL(pathReference);
-      setPdfUri(pdfUrl);
-    } catch (error) {
-      console.error("Error loading PDF:", error);
-    }
-  };
-
   return (
     <View style={styles.container_p}>
-      {pdfVisible ? (
-        // Display the PDF using WebView
-        <WebView source={{ uri: pdfUri }} />
-      ) : (
-        // Show the search bar and search button if pdfVisible is false
-        <View style={styles.searchContainer}>
-          <View
-            style={
-              search !== ""
-                ? styles.searchBar__clicked
-                : styles.searchBar__unclicked
-            }
-          >
-            {/* search Icon */}
-            <Feather
-              name="search"
-              size={20}
-              color="black"
-              style={{ marginLeft: 1 }}
-            />
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
 
-            {/* Input field */}
-            <TextInput
-              style={styles.input}
-              placeholder="Search"
-              value={search}
-              onChangeText={updateSearch}
-              onFocus={() => {
-                setClicked(true);
-              }}
-            />
-            {/* cross Icon, depending on whether the search bar is clicked or not */}
-            {clicked && (
-              <Entypo
-                name="cross"
-                size={20}
-                color="black"
-                style={{ padding: 3, marginLeft: -19 }}
-                onPress={() => {
-                  updateSearch("");
-                  setClicked(false);
-                }}
-              />
-            )}
-          </View>
-        </View>
-      )}
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={search}
+          onChangeText={updateSearch}
+          onFocus={() => {
+            setClicked(true);
+          }}
+        />
+        {/* cross Icon, depending on whether the search bar is clicked or not */}
+        {clicked && (
+          <Entypo
+            name="cross"
+            size={20}
+            color="black"
+            style={{ padding: 3, marginLeft: -19 }}
+            onPress={() => {
+              updateSearch("");
+              setClicked(false);
+            }}
+          />
+        )}
       {clicked && (
         <CustomButton
           title="Search"
